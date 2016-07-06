71dd547495c4c36918584a5c3dc8ba0efe11592b
Rui Botelho <rui.botelho@tecnico.ulisboa.pt>
2016-07-06 11:35:59 +0100

    travis fix

--- setupVagrant.sh
+++ setupVagrant.sh
@@ -2,7 +2,7 @@
 
 # Install the latest version of VirtualBox
 curl -L http://download.virtualbox.org/virtualbox/5.0.20/virtualbox-5.0_5.0.20-106931~Ubuntu~trusty_amd64.deb > virtualbox.deb
-dpkg --force-confnew -i vagrant.deb
+dpkg --force-confnew -i virtualbox.deb
 
 # Install the latest version of Vagrant
 curl -L https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb > vagrant.deb
