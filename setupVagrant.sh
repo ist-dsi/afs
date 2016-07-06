bfd7ab8d4927824a7cc888289de1231d6059b17d
Rui Botelho <rui.botelho@tecnico.ulisboa.pt>
2016-07-06 16:33:10 +0100

    test travis

--- setupVagrant.sh
+++ setupVagrant.sh
@@ -15,5 +15,5 @@ echo "--version:::::::::::::::::::"
 VBoxManage --version
 
 # Install the latest version of Vagrant
-#curl -L https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb > vagrant.deb
-#dpkg --force-confnew -i vagrant.deb
+curl -L https://releases.hashicorp.com/vagrant/1.8.1/vagrant_1.8.1_x86_64.deb > vagrant.deb
+dpkg --force-confnew -i vagrant.deb
